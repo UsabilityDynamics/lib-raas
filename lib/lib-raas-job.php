acb887204c8bb9d4cf94be059a368ed4df43e1ac
Andy Potanin <andy.potanin@usabilitydynamics.com>
2014-05-29 16:27:20 -0400

    organizing

---
+++ lib/lib-raas-job.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * RaaS Handler
+ *
+ * @namespace UsabilityDynamics
+ * @module UsabilityDynamics
+ * @author potanin@UD
+ * @version 0.0.1
+ */
+namespace UsabilityDynamics\RaaS {
+
+  /**
+   * Class Job
+   *
+   * @author team@UD
+   * @version 0.1.1
+   * @class Job
+   * @subpackage Jobs
+   */
+  class Job {
+
+    /**
+     * Jobs Class version.
+     *
+     * @public
+     * @static
+     * @property $version
+     * @type {Object}
+     */
+    public static $version = '0.1.0';
+
+    /**s
+     * Create
+     *
+     * @author potanin@UD
+     * @method __construct
+     * @param $args
+     * @return mixed
+     */
+    public function __construct( $args ) {
+
+
+    }
+
+  }
+
+}
---
+++ lib/lib-raas.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * RaaS Handler
+ *
+ * @namespace UsabilityDynamics
+ * @module UsabilityDynamics
+ * @author potanin@UD
+ * @version 0.0.1
+ */
+namespace UsabilityDynamics {
+
+  /**
+   * Class RaaS
+   *
+   * @author team@UD
+   * @version 0.1.1
+   * @class RaaS
+   * @subpackage RaaSs
+   */
+  final class RaaS {
+
+    /**
+     * RaaSs Class version.
+     *
+     * @public
+     * @static
+     * @property $version
+     * @type {Object}
+     */
+    public static $version = '0.2.1';
+
+  }
+
+}
